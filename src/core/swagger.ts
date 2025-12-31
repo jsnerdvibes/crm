e2f639a9195536a4c719c366936354cf2a73beb8
JS Nerd <shaikhdanish1710@gmail.com>
2025-12-31 19:52:29 +0530

    Solve swagger error

--- src/core/swagger.ts
+++ src/core/swagger.ts
@@ -3,36 +3,86 @@ import swaggerUi from 'swagger-ui-express';
 import { Express } from 'express';
 import { config } from '../config';
 
+
 const options: swaggerJsdoc.Options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'CRM API',
-      version: '1.0.0',
-      description: 'API Documentation for CRM',
+      title: "CRM API",
+      version: "1.0.0",
+      description: "API Documentation for CRM",
     },
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT', // optional, for clarity
-        },
-      },
-    },
-    security: [
-      {
-        bearerAuth: [], // apply globally
-      },
-    ],
+
     servers: [
       {
         url: `http://localhost:${config.app.port}`,
       },
     ],
+
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+
+      responses: {
+        Forbidden: {
+          description: "Forbidden – user not authorized",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  message: {
+                    type: "string",
+                    example: "Unauthorized",
+                  },
+                  data: {
+                    type: "object",
+                    example: {},
+                  },
+                },
+              },
+            },
+          },
+        },
+
+        ServerError: {
+          description: "Internal server error",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  message: {
+                    type: "string",
+                    example: "Something went wrong",
+                  },
+                  data: {
+                    type: "object",
+                    example: {},
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
-  apis: ['./src/modules/**/*.ts'], // <-- scan all modules for JSDoc comments
+
+  apis: ["./src/modules/**/*.ts"],
 };
+
 
 const specs = swaggerJsdoc(options);
 
